a179056e60936d3559930d28a373afd3b1036da0
Sujay-N <sujayn18@gmail.com>
2020-05-24 22:40:15 +0530

    scrollReveal

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+ScrollReveal().reveal('.reveal1',{
+    origin:'top',
+    distance : "20px",
+    delay:500,
+    scale :0.5,
+});
+ScrollReveal().reveal('.reveal2',{delay: 1000 });
+ScrollReveal().reveal('.reveal3',{
+     delay: 1000,
+     origin:'left',
+     distance : "20px",
+     scale :0.5, });
+
+ScrollReveal().reveal('.reveal4',{
+     delay: 1200,
+     origin:'right',
+     distance : "20px",
+     scale :0.4, });
+
+ScrollReveal().reveal('.reveal5',{
+    origin:"top",
+    scale:0.5,
+    distance : "30px",
+    delay: 1000 });
+
+// ScrollReveal().reveal(".card",{
+//     duration:1000,
+//     delay:1000
+// },50)
+ScrollReveal().reveal('.reveal6',{ delay: 1000 });
+
+ScrollReveal().reveal('.reveal7',{
+    origin:'bottom',
+    distance : "20px",
+    delay:1000,
+    scale :0.5, });
